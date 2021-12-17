d668c6212b9155d836cd6ca79c28fcc3b1a04e61
unknown <ovi.chowdhury@hotmail.com>
2021-12-17 22:42:23 +0600

    grid traversal code added

---
+++ algorithms/grid-traversal/grid-traversal.js
@@ -0,0 +1,27 @@
+
+/**
+ * Traverse a n*n matrix using only two moves shift right or shift down
+ * start from 0*0 land in n*n 
+ */
+
+
+function gridTraversal(row, col) {
+    if (row === 0 || col === 0) return 0;
+    if (row === 1 && col === 1) return 1;
+    return gridTraversal(row - 1, col) + gridTraversal(row, col - 1);
+}
+
+function gridTraversalDP(row, col, memo = {}) {
+    const key = row + "=>" + col;
+    if (row === 0 || col === 0) return 0;
+    if (row === 1 && col === 1) return 1;
+    if(key in memo) return memo[key];
+
+    memo[key] = gridTraversalDP(row - 1, col, memo) + gridTraversalDP(row, col - 1, memo);
+    return memo[key];
+}
+
+
+
+console.log(gridTraversal(3, 3));
+console.log(gridTraversalDP(25, 25));
