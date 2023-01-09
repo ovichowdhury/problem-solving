cb3f61a92855de2507263ed5d426f806e9da5e0f
ovichowdhury <ovi.chowdhury@hotmail.com>
2023-01-09 10:13:23 +0600

    leetcode climbing-stairs DP solution added

--- leetcode/climbing-stairs/main.js
+++ leetcode/climbing-stairs/main.js
@@ -4,19 +4,32 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function (n) {
-  let count = 0;
-  const recurse = (num) => {
-    if (num < 0) return;
-    if (num === 0) {
-      count += 1;
-      return;
-    }
-    recurse(num - 1);
-    recurse(num - 2);
-  };
-  recurse(n);
-  return count;
+// var climbStairs = function (n) {
+//   let count = 0;
+//   const recurse = (num) => {
+//     if (num < 0) return;
+//     if (num === 0) {
+//       count += 1;
+//       return;
+//     }
+//     recurse(num - 1);
+//     recurse(num - 2);
+//   };
+//   recurse(n);
+//   return count;
+// };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n, memo = {}) {
+  if (n < 0) return 0;
+  if (n === 0) return 1;
+  if (memo[n] !== undefined) return memo[n];
+  const val = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+  memo[n] = val;
+  return val;
 };
 
-console.log(climbStairs(10));
+console.log(climbStairs(45));
