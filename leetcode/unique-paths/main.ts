0650f1a06b422b74960dbde75cdd1f0bfc79c7ad
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-07-22 01:27:04 +0600

    feat: unique path dp solution added

---
+++ leetcode/unique-paths/main.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/unique-paths/description/
+
+function uniquePaths(m: number, n: number): number {
+  let dp: number[][] = [];
+  for (let i = 0; i < m; i++) {
+    dp[i] = Array(n).fill(0);
+  }
+
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1; // First column
+  }
+
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1; // First row
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // Sum of paths from top and left
+    }
+  }
+
+  return dp[m - 1][n - 1]; // Return the bottom-right corner value
+}
+
+uniquePaths(3, 3); // Example usage, should return 28
