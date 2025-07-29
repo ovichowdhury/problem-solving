18e6bbb65faffd2c57c78693183f4aac20aa1f0a
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-07-30 00:44:56 +0600

    feat: leetcode sql 50 solution added

---
+++ sql/leetcode-50/customer-who-visited-but-did-not-make-any-transactions/main.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select customer_id, count(1) as count_no_trans 
+from visits
+left join transactions
+on visits.visit_id = transactions.visit_id
+where transaction_id is null
+group by customer_id;
