0bb4afba8e1dd2f03f6f99a0701c90f1ad73ddc6
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-10-18 19:06:39 +0600

    feat: leetcode 50 user-activity-for-the-past-30-days-i sol added

---
+++ sql/leetcode-50/user-activity-for-the-past-30-days-i/main.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM 
+    activity
+WHERE 
+    activity_date > '2019-06-27' 
+    AND activity_date <= '2019-07-27'
+GROUP BY 
+    activity_date;
