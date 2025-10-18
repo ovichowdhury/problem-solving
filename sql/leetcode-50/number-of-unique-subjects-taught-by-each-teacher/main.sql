3da8bb89168402f2f069633b9346ede89b003fd1
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-10-18 18:54:44 +0600

    feat: leetcode 50 number-of-unique-subjects-taught-by-each-teacher sol added

---
+++ sql/leetcode-50/number-of-unique-subjects-taught-by-each-teacher/main.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT 
+    teacher_id, 
+    COUNT(DISTINCT subject_id) AS cnt
+FROM 
+    teacher
+GROUP BY 
+    teacher_id;
